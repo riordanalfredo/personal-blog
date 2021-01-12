cee9ccaca8d9082a25640cb8adc6ba505cd30cb6
riordanalfredo <riordan.alfredo@gmail.com>
2021-01-12 14:52:41 +1100

    Add tags in the main post display

---
+++ app/src/components/tags.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Tags = ({ data }) => {
+  const innerStyle = {
+    borderRadius: '1rem',
+    border: '1px solid grey',
+    margin: '0 1px 0 1px',
+    textAlign: 'center',
+    minWidth: '4rem',
+    padding: '0 0.8rem',
+  }
+
+  const outerStyle = {
+    display: `flex`,
+    flexDirection: `row`,
+    justifyContent: `flex-start`,
+  }
+
+  return (
+    <div style={outerStyle}>
+      {data.map(item => {
+        return (
+          <div style={innerStyle}>
+            <small style={{ fontSize: '70%' }}>{item}</small>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export default Tags
