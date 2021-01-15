b8a17d1f6154dfd9715a3c01a58f2736267f4e07
Riordan Dervin Alfredo <riordan.alfredo@gmail.com>
2021-01-15 11:02:18 +1100

    remove undefined category in `resetFilter()` function
    Also, add posts file for redux

--- app/src/containers/filters.js
+++ app/src/containers/filters.js
@@ -3,13 +3,12 @@ import { resetFilter, selectCategory } from '../state/actions/categories'
 import Filters from '../components/filters'
 
 function mapDispatchToProps(dispatch) {
-  console.log(selectCategory)
   return {
     selectCategory: category => {
       dispatch(selectCategory(category))
     },
-    resetFilter: category => {
-      dispatch(resetFilter(category))
+    resetFilter: () => {
+      dispatch(resetFilter())
     },
   }
 }
