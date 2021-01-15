5aaa446fd7e9fda46b9a223914b21d6c46b6274b
Riordan Dervin Alfredo <riordan.alfredo@gmail.com>
2021-01-16 00:18:04 +1100

    update code

--- app/src/pages/about-me/about-text.js
+++ app/src/pages/about-me/about-text.js
@@ -15,18 +15,18 @@ const AboutText = () => (
       complexity that satisfies my hunger for knowledge. The true challenge
       comes not only from the technology side but also from understanding
       business needs. I am proficient in design (
-      <code> UI, UX, architecture </code>), develop, test, and scale software
-      (web &amp; native) applications and/or systems.
+      <code>UI, UX, architecture</code>), develop, test, and scale software (web
+      &amp; native) applications and/or systems.
     </p>
     <p>
       I went to data science &amp; AI area as a Doctor of Philosophy student
-      (with full-ride scholarship) by researching
-      <code> meta-learning &amp; recommender systems </code> topics at Monash
+      (with full-ride scholarship) by researching{' '}
+      <code>meta-learning &amp; recommender systems</code> topics at Monash
       University. Now, I am a teaching associate for software engineering units
       at the Faculty of IT, Monash University. I love connecting with all of my
       students aside from just delivering content. At the same time, I am
       working on various software projects with{' '}
-      <code> ReactJS, Quantlib, NodeJS, Gatsby, and Django frameworks </code>.
+      <code>ReactJS, Quantlib, NodeJS, Gatsby, and Django frameworks</code>.
       <br />
     </p>
   </div>
