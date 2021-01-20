db1a07ac8672c31c9e04343f4ad4ce902b3f1db6
riordanalfredo <riordan.alfredo@gmail.com>
2021-01-20 15:03:56 +1100

    Fix burger-styled

--- app/src/components/burger.js
+++ app/src/components/burger.js
@@ -1,6 +1,6 @@
 // Burger.js
 import React from 'react'
-import { StyledBurger } from './Burger.styled'
+import { StyledBurger } from './burger-styled'
 import { bool, func } from 'prop-types'
 
 const Burger = ({ open, setOpen }) => {
