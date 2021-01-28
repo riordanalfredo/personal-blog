adabab2159ba36dab56f5ae6991cebf19e4ad363
riordanalfredo <riordan.alfredo@gmail.com>
2021-01-28 20:00:51 +1100

    Tried to use localstorage to detect darkmode

--- app/src/components/filter-card.js
+++ app/src/components/filter-card.js
@@ -1,15 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './filter-card.css'
 import './global.css'
+import { ThemeToggler } from 'gatsby-plugin-dark-mode'
 
 const FilterCard = ({ categories, id, selectFunction }) => {
   const { text, style } = categories[id]
   const [isSelected, setIsSelected] = useState(categories[id].isSelected)
+  const [isDark, setIsDark] = useState(false)
 
   const selectCategory = () => {
     setIsSelected(!isSelected)
     selectFunction(id)
   }
+  useEffect(() => {
+    const theme = localStorage.getItem('theme')
+    if (theme !== null) {
+      setIsDark(theme === 'dark' ? true : false)
+    }
+  }, [])
 
   const outerStyle = {
     width: '10vw',
@@ -17,9 +25,12 @@ const FilterCard = ({ categories, id, selectFunction }) => {
     height: 250,
     borderRadius: `10px`,
     cursor: 'pointer',
-    background: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(${style.imgUrl})`,
-    backgroundPosition: 'center top',
-    backgroundSize: 'cover',
+    background: `url(${isDark ? style.imgUrlDark : style.imgUrl})`,
+    backgroundColor: 'white',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: '80%',
+    transition: '0.3s',
   }
   const selectedStyle = {
     ...outerStyle,
