fe2f76eca19c40995601745904220ebe43f16ad4
riordanalfredo <riordan.alfredo@gmail.com>
2021-02-15 18:03:14 +1100

    add the missing icon
    change svg to png

--- app/gatsby-config.js
+++ app/gatsby-config.js
@@ -77,7 +77,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#c39d63`,
         display: `minimal-ui`,
-        icon: `content/assets/logo.svg`,
+        icon: `content/assets/icon.png`,
       },
     },
     `gatsby-plugin-react-helmet`,
